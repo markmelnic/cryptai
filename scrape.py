970e5810f1506c08906f5303d119f14ba88f118a
Mark Melnic <markhappy200@gmail.com>
2021-05-05 11:23:09 +0300

    Test CMC

--- scrape.py
+++ scrape.py
@@ -1,21 +1,11 @@
-from requests import get
-from bs4 import BeautifulSoup
+import json
+from cmc import CMC
 
-BASE_URL = "https://coinmarketcap.com/?page="
-
-for i in range(1, 50):
-    r = get(BASE_URL + str(i))
-    coins = BeautifulSoup(r.content, "html.parser").find(name="tbody")
-    for tr in coins.find_all("tr", recursive=False):
-        coin = tr.find(class_="cmc-link")
-        link = coin['href']
-        details = coin.find_all("div")
-        if len(details) == 0:
-            continue
-
-        logo, name, symbol, rank = details
-        rank = rank.get_text()
-        symbol = symbol.get_text()[len(rank):]
-        name = name.get_text()[:-len(symbol)-len(rank)]
-
-        print(link, rank, name, symbol)
+if __name__ == '__main__':
+    cmc = CMC()
+    cmc.scrape()
+    with open("coins.json", "w") as cfile:
+        coins = {}
+        print(len(cmc.coins))
+        coins['coins'] = [c.dict for c in cmc.coins]
+        json.dump(coins, cfile)
